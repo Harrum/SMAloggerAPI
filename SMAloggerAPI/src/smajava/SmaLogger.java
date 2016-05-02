4d65dd93faff3e159899fe612440462af05a8638
Harm <Harm@pc>
2016-05-02 15:05:10 +0200

    Added a security so that inverters cannot be double detected.

--- SMAloggerAPI/src/smajava/SmaLogger.java
+++ SMAloggerAPI/src/smajava/SmaLogger.java
@@ -3,6 +3,7 @@ package smajava;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import smajava.misc.DEBUG;
 import smajava.misc.VERBOSE;
@@ -11,16 +12,21 @@ public class SmaLogger
 {
 	public static final String VERSION = "0.2 Remaster";
 	
+	public final static long UG_USER = 0x07L;
+	public final static long UG_INSTALLER = 0x0AL;
+	
 	private final String IP_Broadcast = "239.12.255.254";
 	private Ethernet ethernet;
 	private Config config;
 	
+	public static short AppSUSyID;
+	public static long AppSerial;
 	public static int quiet = 0;
 	
-	public SmaLogger()
+	public SmaLogger(Config cfg)
 	{
 		ethernet = new Ethernet();
-		config = new Config();
+		config = cfg;
 	}
 	
 	public int Initialize(String[] args)
@@ -57,6 +63,16 @@ public class SmaLogger
 			System.err.println("Failed to set up socket connection.");
 			return rc;
 		}
+		
+		//Generate a Serial Number for application
+	    AppSUSyID = 125;
+	    Random r = new Random(System.currentTimeMillis());
+	    AppSerial = 900000000 + ((r.nextInt() << 16) + r.nextInt()) % 100000000;
+	    ethernet.AppSUSyID = AppSUSyID;
+	    ethernet.AppSerial = AppSerial;
+	    if (VERBOSE.Normal()) 
+	    	System.out.printf("SUSyID: %d - SessionID: %d (0x%08X)\n", AppSUSyID, AppSerial, AppSerial);
+	    
 		return rc;
 	}
 	
@@ -65,6 +81,23 @@ public class SmaLogger
 		ethernet.Close();
 	}
 	
+	/**
+	 * Used to manually create an inverter with a given ip adres, this method
+	 * gives the inverter a socket connection uses for communication.
+	 * @param ip The ip adress of the inverter.
+	 * @return An inverter with the ip adress and a socket connection.
+	 */
+	public Inverter CreateInverter(String ip)
+	{
+		//Create a new inverter and give it the socket connection.
+		Inverter inv = new Inverter(ip, ethernet);
+		return inv;
+	}
+	
+	/**
+	 * Sends a broadcast message over the network the detect inverters.
+	 * @return A list of found inverters. If no inverters are found, the list is empty.
+	 */
 	public List<Inverter> DetectDevices()
 	{
 		List<Inverter> inverters = new ArrayList<Inverter>();
@@ -93,9 +126,12 @@ public class SmaLogger
 	    		if (quiet == 0) 
 	            	System.out.printf("Inverter IP address: %s found via broadcastidentification\n", ip);
 	
-	    		//Create a new inverter with the ip and add it to the result list.
-	    		Inverter inv = new Inverter(ip);
-	    		inverters.add(inv);
+	    		//Check if the inverter isn't already in the list.
+	    		if(!ContainsInverter(inverters, ip))
+	    		{
+	    			//Create a new inverter with the ip and add it to the result list.
+	    			inverters.add(CreateInverter(ip));
+	    		}	
 	    		foundOne = true;
     		}
     	}
@@ -108,7 +144,7 @@ public class SmaLogger
 			return inverters;
 		}
 
-    	if (DEBUG.NORMAL) 
+    	if (DEBUG.Normal()) 
     		misc.HexDump(CommBuf, bytesRead, 10);
     	
     	return inverters;
@@ -126,5 +162,20 @@ public class SmaLogger
     	ethernet.writeLong(0x00000000);  //Unknown
 
     	ethernet.Send(IP_Broadcast);
+	}
+	
+	private boolean ContainsInverter(List<Inverter> inverters, String ip)
+	{
+		boolean res = false;
+		
+		for(Inverter inv : inverters)
+		{
+			if(inv.GetIP() == ip)
+			{
+				res = true;
+				break;
+			}
+		}
+		return res;
 	}
 }
